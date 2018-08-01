00c8a0689562a60c327659762134b49fb78178fa
arkasya23 <37899483+arkasya23@users.noreply.github.com>
2018-08-01 10:39:09 +0300

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfApp2.Model;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public TextBlock text;
+        public string path = "";
+        public MainWindow()
+        {
+
+            InitializeComponent();
+
+            text = new TextBlock();
+            text.HorizontalAlignment = HorizontalAlignment.Center;
+            text.VerticalAlignment = VerticalAlignment.Center;
+        }
+
+        public void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Weather weather = new Weather();
+            weather.CalculateFields();
+            text.Text = weather.mainTemp.ToString();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Weather weather = new Weather();
+            if (!string.IsNullOrEmpty(countryy.Text))
+            {
+                weather.country = countryy.Text;
+            }
+
+            if (!string.IsNullOrEmpty(cityy.Text))
+            {
+                weather.city = cityy.Text;
+            }
+
+            weather.CalculateFields();
+            mainTemp.Text = weather.mainTemp.ToString();
+            clouds.Text = weather.clouds.ToString();
+            description.Text = weather.description.ToString();
+            minTemp.Text = weather.minTemp.ToString();
+            maxTemp.Text = weather.maxTemp.ToString();
+            humidity.Text = weather.mainHumidity.ToString();
+            pressure.Text = weather.mainPressure.ToString();
+
+            BitmapImage b = new BitmapImage();
+
+            if (weather.mainHumidity > 50)
+            {
+                path = "C:\\Users\\ioana.cirstoiu\\source\\repos\\WpfApp2\\WpfApp2\\Resources\\rainy.png";
+               
+            }
+            else
+            {
+                path = "C:\\Users\\ioana.cirstoiu\\source\\repos\\WpfApp2\\WpfApp2\\Resources\\sunny.png";
+            }
+            b.BeginInit();
+            b.UriSource = new Uri(path);
+            b.EndInit();
+
+            // ... Get Image reference from sender.
+           
+            // ... Assign Source.
+            Image.Source = b;
+            if(weather.mainHumidity > 50)
+            {
+                MessageBox.Show("Ia umbrela", "caption", MessageBoxButton.OK);
+            }
+
+        }
+        private void TextBox_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void Image_Loaded(object sender, RoutedEventArgs e)
+        {
+            BitmapImage b = new BitmapImage();
+            b.BeginInit();
+            if (!string.IsNullOrEmpty(path))
+            {
+                b.UriSource = new Uri(path);
+                b.EndInit();
+
+                // ... Get Image reference from sender.
+                var image = sender as Image;
+                // ... Assign Source.
+                image.Source = b;
+            }
+        }
+
+    }
+}
+ 
+
---
+++ Model/Weather.cs
@@ -0,0 +1,67 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+
+namespace WpfApp2.Model
+{
+    public class Weather
+    {
+        public  string city;
+        public  string country;
+        public static string APIKEY { get; private set; }
+        public string clouds;
+        public string description;
+        public double mainTemp;
+        public double mainPressure;
+        public double mainHumidity;
+        public double minTemp;
+        public double maxTemp;
+        public Weather()
+        {
+            city = "";
+            country = "";
+            clouds = "";
+            description = "";
+            mainHumidity = 0;
+            mainTemp = 0;
+            minTemp = 0;
+            maxTemp = 0;
+            mainPressure = 0;
+
+        }
+        public void CalculateFields()
+        {
+            APIKEY = "8ca63c98ee859769cf5318d36ff6574e";
+            string result;
+            JObject jsonResult;
+            String st = $"http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID={APIKEY}";
+            String st3 = $"http://api.openweathermap.org/data/2.5/weather?q=Bucharest,ro&APPID={APIKEY}";
+            String st2 = $"http://api.openweathermap.org/data/2.5/weather?q={this.city},{this.country}&APPID={APIKEY}";
+            using (WebClient wc = new WebClient())
+            {
+                var json = wc.DownloadString(st2);
+                result = json.ToString();
+                jsonResult = JObject.Parse(json);
+
+            }
+
+            JArray mainWeather = (JArray)jsonResult["weather"];
+            clouds = mainWeather[0]["main"].ToString();
+            description = mainWeather[0]["description"].ToString();
+
+
+            mainTemp = jsonResult["main"]["temp"].Value<double>();
+            mainPressure = jsonResult["main"]["pressure"].Value<double>();
+            mainHumidity = jsonResult["main"]["humidity"].Value<double>();
+            minTemp = jsonResult["main"]["temp_min"].Value<double>();
+            maxTemp = jsonResult["main"]["temp_max"].Value<double>();
+
+            mainTemp -= 273.0;
+            minTemp -= 273.0;
+            maxTemp -= 273.0;
+        }
+    }
+}
